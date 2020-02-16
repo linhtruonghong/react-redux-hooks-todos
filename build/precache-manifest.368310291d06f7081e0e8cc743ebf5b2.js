46f45310ff5135138a2e4eadb0507ea7a6f93098
Linh.Truong <Linh.Hong@cosector.com>
2020-02-16 13:30:34 +0700

    builded

---
+++ build/precache-manifest.368310291d06f7081e0e8cc743ebf5b2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c884caa0233399fa45e90305ec2481a4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "83662243f57ee6798460",
+    "url": "/static/js/2.b492a0de.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.b492a0de.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a1dd44f451c59fbee313",
+    "url": "/static/js/main.6613a289.chunk.js"
+  },
+  {
+    "revision": "d125de010b993a99e2d2",
+    "url": "/static/js/runtime-main.d60e8bc1.js"
+  }
+]);
